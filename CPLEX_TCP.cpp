b5536f690a839c64a56a0026d0475cb17e3738d3
FELIPE-DESKTOP\felip <felipeliborio@outlook.com>
2018-05-20 23:48:47 -0300

    Tentativa de adição de restrições para elos e roteadores.

--- CPLEX_TCP.cpp
+++ CPLEX_TCP.cpp
@@ -7,24 +7,110 @@ FInstance CPLEX_TCP(FInstance Instance)
 		IloModel Model(Environment);
 		IloCplex Cplex(Model);
 
-		std::deque<std::deque<int>> GraphM = Instance.GetAdjacencyMatrix();
 
-
-		IloArray<IloBoolVarArray> Belongs (Environment, GraphM.size());
-		for (int i = 0; i < GraphM.size(); i++) {
-			Belongs[i] = IloBoolVarArray(Environment, GraphM.size());
+		//Adding the objective and related variables*********************************
+		std::deque<std::deque<int>> w = Instance.GetAdjacencyMatrix();//Weight matrix
+		IloArray<IloBoolVarArray> y(Environment, w.size());//Membership matrix
+		for (int i = 0; i < w.size(); i++) {
+			y[i] = IloBoolVarArray(Environment, w.size());
 		}
 
 		IloExpr WeightSum(Environment);
 
-		for (int i = 0; i < GraphM.size(); i++) {
-			for (int j = 0; j < GraphM.size(); j++){
-				WeightSum += GraphM[i][j] * GraphM[i][j];
+		for (int i = 0; i < w.size(); i++) {
+			for (int j = 0; j < w.size(); j++) {
+				WeightSum += w[i][j] * y[i][j];
 			}
 		}
 
 		IloObjective Objective = IloMinimize(Environment, WeightSum);
 		Model.add(Objective);
+		//***************************************************************************
+		
+		std::deque<int> Y;//Optional vertices
+		Y.resize(w.size(), 1);
+		auto Terminals = Instance.GetTerminals();
+		for (auto Terminal : Terminals) {
+			Y[Terminal] = 0;
+		}
+		/*
+		IloBoolVarArray l(Environment, Y.size());//'is link' list
+		IloBoolVarArray r(Environment, Y.size());//'is router' list
+		*/
+		
+		/*
+		IloExpr LinkSum(Environment);
+
+		for (int i = 0; i < Y.size(); i++) {
+				LinkSum += l[i];
+		}
+		Model.add(LinkSum <= Instance.GetMaxLinks() + Instance.GetMaxRouters());
+		*/
+		
+		//Adding links and routers restrictions**************************************
+		IloExpr LinkCount(Environment);
+		IloExpr RouterCount(Environment);
+		for (int i = 0; i < Y.size(); i++) {
+			if (std::find(Instance.GetTerminals().begin(), 
+				Instance.GetTerminals().end(), i) == Instance.GetTerminals().end()) {
+				IloExpr Degree(Environment);
+				for (int j = 0; j < y[i].getSize(); j++) Degree += y[i][j];
+				if (Degree.asVariable == 2) {
+					LinkCount += 1;
+				}
+				else if (Degree.asVariable > 2) {
+					RouterCount += 1;
+				}
+			}
+		}
+		Model.add(LinkCount <= Instance.GetMaxLinks());
+		Model.add(RouterCount <= Instance.GetMaxRouters());
+		//***************************************************************************
+
+
+		IloArray<IloBoolVarArray> Belongs(Environment, GraphM.size());
+		IloArray<IloArray<IloBoolVarArray> > x(Environment, GraphM.size());
+
+		for (int i = 0; i < GraphM.size(); i++) {
+			Belongs[i] = IloBoolVarArray(Environment, GraphM.size());
+			Belongs[i] = IloArray<IloBoolVarArray>(Environment, GraphM.size());
+			for (int j = 0; j < GraphM.size(); j++) {
+				x[i][j] = IloBoolVarArray(Environment, QNT_TER - 1); // O tamanho de S0
+			}
+		}
+
+		IloExpr WeightSum(Environment);
+
+		for (int i = 0; i < GraphM.size(); i++) {
+			for (int j = 0; j < GraphM.size(); j++) {
+				WeightSum += GraphM[i][j] * Belongs[i][j];
+			}
+		}
+
+
+		for (int k : S0) {
+			if (k == s) continue;
+
+			for (int j = 0; j < GraphM.size(); j++) {
+				if (j == s or j == k) continue;
+
+				IloExpr SumOut(Environment);
+				for (int i = 0; i < GraphM.size(); i++) {
+					SumOut += x[i][j][k];
+				}
+				IloExpr SumIn(Environment);
+				for (int i = 0; i < GraphM.size(); i++) {
+					SumIn += x[j][i][k];
+				}
+
+				Model.add(SumOut == SumIn);
+			}
+		}
+
+
+		IloObjective Objective = IloMinimize(Environment, WeightSum);
+		Model.add(Objective);
+
 
 		/*
 		Exemplos
@@ -35,7 +121,7 @@ FInstance CPLEX_TCP(FInstance Instance)
 
 		IloNumArray NumArray(env, numCoef, c1, c2, ..., c6);
 		IloNumArray NumArray(Environment, 3, 10, 11, 12);
-		
+
 		IloScalProd(VetorDeVariaveis, VetorDeCoeficientes);
 
 		*/
@@ -44,12 +130,12 @@ FInstance CPLEX_TCP(FInstance Instance)
 		/*
 		Exemplo 1
 		IloNumVar X11(Environment), X12(Environment), X13(Environment),
-			X21(Environment), X22(Environment), X23(Environment);
+		X21(Environment), X22(Environment), X23(Environment);
 
-		Model.add(IloMinimize(Environment, 4 * X11 + 2 * X12 + 5 * X13 + 
+		Model.add(IloMinimize(Environment, 4 * X11 + 2 * X12 + 5 * X13 +
 		11 * X21 + 7 * X22 + 4 * X23));
 
-		
+
 		Model.add(X11 + X12 + X13 <= 800);
 		Model.add(X21 + X22 + X23 <= 1000);
 		Model.add(X11 + X21		  == 500);
